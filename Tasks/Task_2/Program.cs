bf03cbd433815c20c4a865d056e2f9c591da7593
Alexander Furmanov <kymapcah61@gmail.com>
2022-08-19 23:00:55 +0300

    Finish task 2

---
+++ Tasks/Task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿void FindeIndex(string[] array)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            Console.Write($"{array[i]}, ");
+        }
+    }
+}
+string[] array = { "1234", "1567", "-2", "computer science" };
+FindeIndex(array);
